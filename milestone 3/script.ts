d51ce7b0cc9822bdf4b4e278c9c70ffc4a774574
Amraha Anwar <160616387+Amraha-Anwar@users.noreply.github.com>
2024-12-18 00:57:14 +0500

    Add files via upload

---
+++ milestone 3/script.ts
@@ -0,0 +1,44 @@
+// import form and display resume
+
+const form = document.getElementById("resume-form") as HTMLFormElement;
+const resumeDisplayElement= document.getElementById('resume-display') as HTMLDivElement;
+
+// form submission
+
+form.addEventListener('submit' , (event: Event) =>{
+    event.preventDefault();  //prevent page reload
+
+    // collect input values
+    const name= (document.getElementById('name') as HTMLInputElement).value
+    const phone= (document.getElementById('phone') as HTMLInputElement).value
+    const email= (document.getElementById('email') as HTMLInputElement).value
+    const education= (document.getElementById('education') as HTMLInputElement).value
+    const experience= (document.getElementById('experience') as HTMLInputElement).value
+    const skills= (document.getElementById('skills') as HTMLInputElement).value
+
+    // generate the dynamic resume content
+
+    const resumeHTML =`
+    <h2><b>Resume</b></h2>
+    <h3>Personal Information</h3>
+    <p><b>Name:</b>${name}</p>
+     <p><b>Name:</b>${phone}</p>
+      <p><b>Name:</b>${email}</p>
+
+      <h3>Education</h3>
+      <p>${education}</p>
+
+      <h3>Experience</h3>
+      <p>${experience}</p>
+
+       <h3>Education</h3>
+      <p>${education}</p>
+       `;
+
+    // generated resume 
+    if (resumeDisplayElement){
+        resumeDisplayElement.innerHTML = resumeHTML;
+    }else{
+        console.error('The resume display element is missing.')
+    }
+});
